f7e30fd656608b3adf1c41df8a61b404e52f6153
梅林的法杖 <530264127@qq.com>
2020-03-22 14:57:39 +0800

    wx

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 //App.js
 import React from "react";
+import { Provider } from "react-redux";
 import { IconStyle } from "./assets/iconfont/iconfont";
 import { GlobalStyle } from "./style";
 import { renderRoutes } from "react-router-config";
+import store from "./store/index";
 import routes from "./routes/index";
 import { HashRouter } from "react-router-dom";
 
 function App() {
   return (
-    <HashRouter>
-      <GlobalStyle></GlobalStyle>
-      <IconStyle></IconStyle>
-      {renderRoutes(routes)}
-    </HashRouter>
+    <Provider store={store}>
+      <HashRouter>
+        <GlobalStyle></GlobalStyle>
+        <IconStyle></IconStyle>
+        {renderRoutes(routes)}
+      </HashRouter>
+    </Provider>
   );
 }
 
---
+++ src/store/reducer.js
@@ -0,0 +1,7 @@
+// reducer.js
+
+import { combineReducers } from "redux-immutable";
+
+export default combineReducers({
+  // 之后开发具体功能模块的时候添加 reducer
+});
