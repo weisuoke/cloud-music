7692e0ec9f391abce494fc1fb5826481e195f113
梅林的法杖 <530264127@qq.com>
2020-04-04 21:14:47 +0800

    wx

--- src/application/Singers/index.js
+++ src/application/Singers/index.js
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useState } from 'react'
+import Horizen from '../../baseUI/horizon-item'
+import { categoryTypes, alphaTypes } from '../../api/config'
+import { NavContainer } from './styles'
 
-function Singers(props) {
-  return <div>Singers</div>;
+function Singers (props) {
+  let [category, setCategory] = useState('')
+  let [alpha, setAlpha] = useState('')
+
+  let handleUpdateAlpha = (val) => {
+    setAlpha(val)
+  }
+
+  let handleUpdateCatetory = (val) => {
+    setCategory(val)
+  }
+
+  return (
+    <NavContainer>
+      <Horizen
+        list={categoryTypes}
+        title={'分类 (默认热门):'}
+        handleClick={handleUpdateCatetory}
+        oldVal={category}/>
+      <Horizen
+        list={alphaTypes}
+        title={'首字母:'}
+        handleClick={val => handleUpdateAlpha(val)}
+        oldVal={alpha}/>
+    </NavContainer>
+  )
 }
 
-export default React.memo(Singers);
+export default React.memo(Singers)
