bfc7f5b958b7baa169b95ae5cb26b954a95c2a61
梅林的法杖 <530264127@qq.com>
2020-04-05 22:45:37 +0800

    wx

--- src/application/Player/index.js
+++ src/application/Player/index.js
@@ -9,10 +9,18 @@ import {
   changePlayMode,
   changeFullScreen
 } from './store/actionCreators'
+import MiniPlayer from './miniPlayer'
 
 function Player(props) {
+  const currentSong = {
+    al: { picUrl: 'https://p1.music.126.net/JL_id1CFwNJpzgrXwemh4Q==/109951164172892390.jpg' },
+    name: '木偶人',
+    ar: [{ name: '薛之谦' }]
+  }
   return (
-    <div>Player</div>
+    <div>
+      <MiniPlayer song={currentSong}/>
+    </div>
   )
 }
 
