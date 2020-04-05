46b20dc29c77ca6811d1147f1d01edb30660a979
梅林的法杖 <530264127@qq.com>
2020-04-05 14:50:30 +0800

    wx

--- src/api/utils.js
+++ src/api/utils.js
@@ -1,3 +1,4 @@
+import { RankTypes } from './config'
 // 大家按照这个目录层级新建文件
 //src/api/utils.js
 export const getCount = count => {
@@ -22,4 +23,21 @@ export const debounce = (func, delay) => {
       clearTimeout(timer)
     }, delay)
   }
+}
+
+//处理数据，找出第一个没有歌名的排行榜的索引
+export const filterIndex = rankList => {
+  for (let i = 0; i < rankList.length - 1; i++) {
+    if (rankList[i].tracks.length && !rankList[i + 1].tracks.length) {
+      return i + 1
+    }
+  }
+}
+
+//找出排行榜的编号
+export const filterIdx = name => {
+  for (var key in RankTypes) {
+    if (RankTypes[key] === name) return key
+  }
+  return null
 }
