3830dcb0a1d3d9d8ab484351670a90b571f2df2b
梅林的法杖 <530264127@qq.com>
2020-04-10 22:08:14 +0800

    wx

--- src/application/Album/index.js
+++ src/application/Album/index.js
@@ -1,55 +1,61 @@
-import React, { useState, useCallback, useRef, useEffect } from 'react';
-import { Container, TopDesc, Menu, SongList, SongItem } from './style';
-import { CSSTransition } from 'react-transition-group';
-import Header from './../../baseUI/header/index';
-import Scroll from '../../baseUI/scroll/index';
-import { getCount, isEmptyObject } from '../../api/utils';
-import { getName } from './../../api/utils';
-import { HEADER_HEIGHT } from './../../api/config';
-import style from "../../assets/global-style";
-import { connect } from 'react-redux';
-import { getAlbumList, changeEnterLoading } from './store/actionCreators';
-import Loading from '../../baseUI/loading/index';
+import React, { useState, useCallback, useRef, useEffect } from 'react'
+import { Container, TopDesc, Menu } from './style'
+import { CSSTransition } from 'react-transition-group'
+import Header from './../../baseUI/header/index'
+import Scroll from '../../baseUI/scroll/index'
+import { getCount, isEmptyObject } from '../../api/utils'
+import { HEADER_HEIGHT } from './../../api/config'
+import style from '../../assets/global-style'
+import { connect } from 'react-redux'
+import { getAlbumList, changeEnterLoading } from './store/actionCreators'
+import Loading from '../../baseUI/loading/index'
+import SongsList from '../SongsList'
+import MusicNote from '../../baseUI/music-note/index'
 
 function Album(props) {
-  const [showStatus, setShowStatus] = useState(true);
-  const [title, setTitle] = useState("歌单");
-  const [isMarquee, setIsMarquee] = useState(false);//是否跑马灯
+  const [showStatus, setShowStatus] = useState(true)
+  const [title, setTitle] = useState('歌单')
+  const [isMarquee, setIsMarquee] = useState(false)//是否跑马灯
 
-  const headerEl = useRef();
+  const headerEl = useRef()
+  const musicNoteRef = useRef()
 
-  const { currentAlbum: currentAlbumImmutable, enterLoading } = props;
-  const { getAlbumDataDispatch } = props;
+  const { currentAlbum: currentAlbumImmutable, enterLoading, songsCount } = props
+  const { getAlbumDataDispatch } = props
 
-  const id = props.match.params.id;
+  const id = props.match.params.id
 
   useEffect(() => {
-    getAlbumDataDispatch(id);
-  }, [getAlbumDataDispatch, id]);
+    getAlbumDataDispatch(id)
+  }, [getAlbumDataDispatch, id])
 
-  let currentAlbum = currentAlbumImmutable.toJS();
+  let currentAlbum = currentAlbumImmutable.toJS()
 
   const handleBack = useCallback(() => {
-    setShowStatus(false);
-  }, []);
+    setShowStatus(false)
+  }, [])
 
   const handleScroll = useCallback((pos) => {
-    let minScrollY = -HEADER_HEIGHT;
-    let percent = Math.abs(pos.y / minScrollY);
-    let headerDom = headerEl.current;
+    let minScrollY = -HEADER_HEIGHT
+    let percent = Math.abs(pos.y / minScrollY)
+    let headerDom = headerEl.current
     //滑过顶部的高度开始变化
     if (pos.y < minScrollY) {
-      headerDom.style.backgroundColor = style["theme-color"];
-      headerDom.style.opacity = Math.min(1, (percent - 1) / 2);
-      setTitle(currentAlbum.name);
-      setIsMarquee(true);
+      headerDom.style.backgroundColor = style['theme-color']
+      headerDom.style.opacity = Math.min(1, (percent - 1) / 2)
+      setTitle(currentAlbum.name)
+      setIsMarquee(true)
     } else {
-      headerDom.style.backgroundColor = "";
-      headerDom.style.opacity = 1;
-      setTitle("歌单");
-      setIsMarquee(false);
+      headerDom.style.backgroundColor = ''
+      headerDom.style.opacity = 1
+      setTitle('歌单')
+      setIsMarquee(false)
     }
-  }, [currentAlbum]);
+  }, [currentAlbum])
+
+  const musicAnimation = (x, y) => {
+    musicNoteRef.current.startAnimation({ x, y })
+  }
 
   const renderTopDesc = () => {
     return (
@@ -59,7 +65,7 @@ function Album(props) {
         </div>
         <div className="img_wrapper">
           <div className="decorate"></div>
-          <img src={currentAlbum.coverImgUrl} alt="" />
+          <img src={currentAlbum.coverImgUrl} alt=""/>
           <div className="play_count">
             <i className="iconfont play">&#xe885;</i>
             <span className="count">{getCount(currentAlbum.subscribedCount)}</span>
@@ -69,7 +75,7 @@ function Album(props) {
           <div className="title">{currentAlbum.name}</div>
           <div className="person">
             <div className="avatar">
-              <img src={currentAlbum.creator.avatarUrl} alt="" />
+              <img src={currentAlbum.creator.avatarUrl} alt=""/>
             </div>
             <div className="name">{currentAlbum.creator.nickname}</div>
           </div>
@@ -99,40 +105,6 @@ function Album(props) {
         </div>
       </Menu>
     )
-  };
-
-  const renderSongList = () => {
-    return (
-      <SongList>
-        <div className="first_line">
-          <div className="play_all">
-            <i className="iconfont">&#xe6e3;</i>
-            <span>播放全部 <span className="sum">(共{currentAlbum.tracks.length}首)</span></span>
-          </div>
-          <div className="add_list">
-            <i className="iconfont">&#xe62d;</i>
-            <span>收藏({getCount(currentAlbum.subscribedCount)})</span>
-          </div>
-        </div>
-        <SongItem>
-          {
-            currentAlbum.tracks.map((item, index) => {
-              return (
-                <li key={index}>
-                  <span className="index">{index + 1}</span>
-                  <div className="info">
-                    <span>{item.name}</span>
-                    <span>
-                      {getName(item.ar)} - {item.al.name}
-                    </span>
-                  </div>
-                </li>
-              )
-            })
-          }
-        </SongItem>
-      </SongList>
-    )
   }
 
   return (
@@ -144,7 +116,7 @@ function Album(props) {
       unmountOnExit
       onExited={props.history.goBack}
     >
-      <Container>
+      <Container play={songsCount}>
         <Header ref={headerEl} title={title} handleClick={handleBack} isMarquee={isMarquee}></Header>
         {!isEmptyObject(currentAlbum) ?
           (
@@ -153,15 +125,22 @@ function Album(props) {
               onScroll={handleScroll}
             >
               <div>
-                { renderTopDesc() }
-                { renderMenu() }
-                { renderSongList() }
+                {renderTopDesc()}
+                {renderMenu()}
+                <SongsList
+                  songs={currentAlbum.tracks}
+                  collectCount={currentAlbum.subscribedCount}
+                  showCollect={true}
+                  showBackground={true}
+                  musicAnimation={musicAnimation}
+                ></SongsList>
               </div>
             </Scroll>
           )
           : null
         }
-        { enterLoading ? <Loading></Loading> : null}
+        {enterLoading ? <Loading></Loading> : null}
+        <MusicNote ref={musicNoteRef}></MusicNote>
       </Container>
     </CSSTransition>
   )
@@ -171,16 +150,17 @@ function Album(props) {
 const mapStateToProps = (state) => ({
   currentAlbum: state.getIn(['album', 'currentAlbum']),
   enterLoading: state.getIn(['album', 'enterLoading']),
-});
+  songsCount: state.getIn(['player', 'playList']).size
+})
 // 映射dispatch到props上
 const mapDispatchToProps = (dispatch) => {
   return {
     getAlbumDataDispatch(id) {
-      dispatch(changeEnterLoading(true));
-      dispatch(getAlbumList(id));
+      dispatch(changeEnterLoading(true))
+      dispatch(getAlbumList(id))
     },
   }
-};
+}
 
 // 将ui组件包装成容器组件
-export default connect(mapStateToProps, mapDispatchToProps)(React.memo(Album));
+export default connect(mapStateToProps, mapDispatchToProps)(React.memo(Album))
